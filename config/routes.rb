f8cc34294c6dab723d7e23a49e38a83eb131d3fa
matheusbarancelli <mbarancelli@hotmail.com>
2020-02-18 17:27:41 -0300

    qse pronto

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'tasks/new', to: 'tasks#new'
+  get 'tasks', to: 'tasks#index'
+
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_tasks'
+  delete 'tasks/:id', to: 'tasks#destroy'
+  patch 'tasks/:id', to: 'tasks#update'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
 end
